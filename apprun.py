294cd514da7c58d92f4c9ead821dd78e4c994a89
SaurabhUpadhyay2001 <89247071+SaurabhUpadhyay2001@users.noreply.github.com>
2021-08-20 16:05:50 +0530

    Update apprun.py

--- apprun.py
+++ apprun.py
@@ -1,14 +1,19 @@
 import json
 path = r"F:\DRC Practical Round\test.json"
-path_o = r"F:\DRC Practical Round\output.json"
-def rec():
-    with open(path, 'r') as f:
-        body = f.read()
-    if body=="":
-        return 0
+path_o = r"F:\DRC Practical Round\output1.json"
+list1=[]
+def change(body):
+    if len(body)==0:
+        return list1
     else:
-        with open(path_o, 'w') as f:
-            f.write(str(body).replace("+type", "+size@5+type"))
-    rec()
+        a=body[0]
+        b=a.replace('+type','+size@5+type')            #replacing 1st element everytime
+        list1.append(b)
+        body.pop(0)
+        return change(body)             #Recursive Body call
 
-rec()
+con=open(path,'r')
+body=con.readlines()      #all content is in body
+a=change(body)
+abc=open(path_o,'w')
+abc.writelines(a)          #writing all changes to output file
